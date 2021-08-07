f0a123e3af481c62eb18193c817fd384dc1b1f1c
naomi <kiwijang5473@gmail.com>
2021-08-08 00:13:51 +0800

    刪除沒用到的Task.Yield()、修改非同步的地方

--- SkImageResizer/SKImageProcess.cs
+++ SkImageResizer/SKImageProcess.cs
@@ -53,29 +53,35 @@ namespace SkImageResizer
         Directory.CreateDirectory(destPath);
       }
 
-      await Task.Yield();
-
       var allFiles = FindImages(sourcePath);
+
+      List<Task> tasks = new List<Task>();
       foreach (var filePath in allFiles)
       {
-        var bitmap = SKBitmap.Decode(filePath);
-        var imgPhoto = SKImage.FromBitmap(bitmap);
-        var imgName = Path.GetFileNameWithoutExtension(filePath);
+        Task t = Task.Run(() =>
+        {
+          var bitmap = SKBitmap.Decode(filePath);
+          var imgPhoto = SKImage.FromBitmap(bitmap);
+          var imgName = Path.GetFileNameWithoutExtension(filePath);
 
-        var sourceWidth = imgPhoto.Width;
-        var sourceHeight = imgPhoto.Height;
+          var sourceWidth = imgPhoto.Width;
+          var sourceHeight = imgPhoto.Height;
 
-        var destinationWidth = (int)(sourceWidth * scale);
-        var destinationHeight = (int)(sourceHeight * scale);
+          var destinationWidth = (int)(sourceWidth * scale);
+          var destinationHeight = (int)(sourceHeight * scale);
 
-        using var scaledBitmap = bitmap.Resize(
+          using var scaledBitmap = bitmap.Resize(
             new SKImageInfo(destinationWidth, destinationHeight),
             SKFilterQuality.High);
-        using var scaledImage = SKImage.FromBitmap(scaledBitmap);
-        using var data = scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
-        using var s = File.OpenWrite(Path.Combine(destPath, imgName + ".jpg"));
-        data.SaveTo(s);
+          using var scaledImage = SKImage.FromBitmap(scaledBitmap);
+          using var data = scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
+          using var s = File.OpenWrite(Path.Combine(destPath, imgName + ".jpg"));
+          data.SaveTo(s);
+        });
+        tasks.Add(t);
       }
+
+      await Task.WhenAll(tasks);
     }
 
     /// <summary>
